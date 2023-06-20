f862ce1000ee0d7fa47051dee969b169c3acdc83
余半盏 <49603999+ycq0125@users.noreply.github.com>
2023-06-20 17:58:00 +0800

    fix xiaohongxhu  captcha

--- rotate_captcha.py
+++ rotate_captcha.py
@@ -7,24 +7,42 @@
 @File    : rotate_captcha.py
 @Software: PyCharm
 """
-import time
-
 import cv2.cv2 as cv2
 import math
 import numpy as np
 
 
+# def circle_point_px(img, accuracy_angle, r=None):
+#     rows, cols, channel = img.shape
+#     assert 360 % accuracy_angle == 0
+#     x0, y0 = r0, _ = (rows // 2, cols // 2)
+#     if r: r0 = r
+#     circle_px_list = []
+#     for angle in np.arange(0, 360, accuracy_angle):
+#         # 圆上点 x=x0 + r*cosθ; y=y0 + r*sinθ
+#         x = x0 + r0 * math.cos(angle / 180 * math.pi)
+#         y = y0 + r0 * math.sin(angle / 180 * math.pi)
+#         # print(angle, x, y)
+#         circle_px_list.append(img[int(x)][int(y)])
+#     return circle_px_list
+
 def circle_point_px(img, accuracy_angle, r=None):
-    rows, cols, channel = img.shape
+    rows, cols, _ = img.shape
     assert 360 % accuracy_angle == 0
     x0, y0 = r0, _ = (rows // 2, cols // 2)
     if r: r0 = r
-    circle_px_list = []
-    for angle in np.arange(0, 360, accuracy_angle):
-        # 圆上点 x=x0 + r*cosθ; y=y0 + r*sinθ
-        x = x0 + r0 * math.cos(angle / 180 * math.pi)
-        y = y0 + r0 * math.sin(angle / 180 * math.pi)
-        circle_px_list.append(img[int(x)][int(y)])
+
+    angles = np.arange(0, 360, accuracy_angle)
+    cos_angles = np.cos(np.deg2rad(angles))
+    sin_angles = np.sin(np.deg2rad(angles))
+
+    x = x0 + r0 * cos_angles
+    y = y0 + r0 * sin_angles
+
+    x = np.round(x).astype(int)
+    y = np.round(y).astype(int)
+
+    circle_px_list = img[x, y]
     return circle_px_list
 
 
@@ -50,17 +68,19 @@ def HSVDistance(c1, c2):
     return rlt
 
 
-def discern(inner_image_brg, outer_image_brg, result_img=None, angle=4, default_dis=5):
+def discern(inner_image_brg, outer_image_brg, result_img=None, ):
     inner_image_brg = cv2.imread(inner_image_brg)
     outer_image_brg = cv2.imread(outer_image_brg)
     inner_image = cv2.cvtColor(inner_image_brg, cv2.COLOR_BGR2HSV)
     outer_image = cv2.cvtColor(outer_image_brg, cv2.COLOR_BGR2HSV)
+    # cv2.imshow('inner_source', inner_image)
+    # cv2.imshow('outer_source', outer_image)
     all_deviation = []
     for result in range(0, 180):
         inner = rotate(inner_image, -result)  # 顺时针
         outer = rotate(outer_image, result)
-        inner_circle_point_px = circle_point_px(inner, angle, (inner.shape[0] // 2) - default_dis)
-        outer_circle_point_px = circle_point_px(outer, angle, (inner.shape[0] // 2) + default_dis)
+        inner_circle_point_px = circle_point_px(inner, 1, (inner.shape[0] // 2) - 5)
+        outer_circle_point_px = circle_point_px(outer, 1, (inner.shape[0] // 2) + 5)
         total_deviation = 0
         for i in range(len(inner_circle_point_px)):
             in_px = inner_circle_point_px[i]
@@ -72,15 +92,62 @@ def discern(inner_image_brg, outer_image_brg, result_img=None, angle=4, default_
     print(result)
 
     if result_img:
+        # result = 158
         inner = rotate(inner_image_brg, -result)  # 顺时针
         outer = rotate(outer_image_brg, result)
         left_point = outer.shape[0] / 2 - inner.shape[0] / 2
         right_point = outer.shape[0] / 2 + inner.shape[0] / 2
         replace_area = outer[int(left_point):int(right_point), int(left_point):int(right_point)]
         outer[int(left_point):int(right_point), int(left_point):int(right_point)] = replace_area + inner
+        # cv2.imshow('result', outer)
         cv2.imwrite(result_img, outer)
     return result
 
+def crop_to_square(image):
+    height, width = image.shape[:2]
+    size = min(height, width)
+    start_y = (height - size) // 2
+    start_x = (width - size) // 2
+    cropped = image[start_y:start_y+size, start_x:start_x+size]
+    return cropped
+
+def single_discern(inner_image_brg, outer_image_brg, result_img=None, ):
+    inner_image_brg = cv2.imread(inner_image_brg)
+    outer_image_brg = cv2.imread(outer_image_brg)
+    inner_image = cv2.cvtColor(inner_image_brg, cv2.COLOR_BGR2HSV)
+    outer_image = cv2.cvtColor(outer_image_brg, cv2.COLOR_BGR2HSV)
+    outer_image = crop_to_square(outer_image)
+    # cv2.imshow('inner_source', inner_image)
+    # cv2.imshow('outer_source', outer_image)
+    all_deviation = []
+    for result in range(0, 180):
+        inner = rotate(inner_image, -result)  # 顺时针
+        outer = rotate(outer_image, 0)
+        inner_circle_point_px = circle_point_px(inner, 1, 95)
+        outer_circle_point_px = circle_point_px(outer, 1, 105)
+        total_deviation = 0
+        for i in range(len(inner_circle_point_px)):
+            in_px = inner_circle_point_px[i]
+            out_px = outer_circle_point_px[i]
+            deviation = HSVDistance(in_px, out_px)
+            total_deviation += deviation
+        all_deviation.append(total_deviation)
+    result = all_deviation.index(min(all_deviation))
+    print(result)
+
+    if result_img:
+        # result = 158
+        inner = rotate(inner_image_brg, -result)  # 顺时针
+        outer = rotate(outer_image_brg, 0)
+        outer = crop_to_square(outer)
+        left_point = outer.shape[0] / 2 - inner.shape[0] / 2
+        right_point = outer.shape[0] / 2 + inner.shape[0] / 2
+        replace_area = outer[int(left_point):int(right_point), int(left_point):int(right_point)]
+        outer[int(left_point):int(right_point), int(left_point):int(right_point)] = replace_area + inner
+        # cv2.imshow('result', outer)
+        cv2.imwrite(result_img, outer)
+    return result
 
 if __name__ == '__main__':
-    discern('./imgs/inner_4.png', './imgs/outer_4.png', './imgs/result.png')
+    # discern('./imgs/inner_8.png', './imgs/outer_8.png', './imgs/result.png')
+    single_discern('./imgs/inner.png', './imgs/outer.png', './imgs/result.png')
